bcfb7def87ee63daf17e8711dd33fcc137f6daec
清梦 <157796080+QIngMengK@users.noreply.github.com>
2025-08-14 10:14:00 +0800

    创建新的main函数分支

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+//
+// Created by 清梦 on 2025/8/14.
+//
 #include<iostream>
 using namespace std;
 
-int main()
-{
-  
+int main(){
 
 
-  return 0;
+    return 0;
 }
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by 清梦 on 2025/8/14.
+//
+
+#ifndef RPGSTORYCONTROLLER_MAIN_H
+#define RPGSTORYCONTROLLER_MAIN_H
+
+
+class main {
+};
+
+
+#endif //RPGSTORYCONTROLLER_MAIN_H
